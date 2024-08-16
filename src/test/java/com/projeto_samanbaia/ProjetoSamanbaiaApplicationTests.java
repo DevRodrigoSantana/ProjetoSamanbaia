600b2486f1214da1a8afd52e24b5a91eeab05158
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2024-08-16 17:21:41 -0300

    mudando na raiz

--- src/test/java/com/projeto_samanbaia/ProjetoSamanbaiaApplicationTests.java
+++ src/test/java/com/projeto_samanbaia/ProjetoSamanbaiaApplicationTests.java
@@ -2,8 +2,10 @@ package com.projeto_samanbaia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan(basePackages = {"com.projeto_samanbaia", "socket"})
 class ProjetoSamanbaiaApplicationTests {
 
 	@Test
